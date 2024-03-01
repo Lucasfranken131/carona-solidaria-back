d7a1842c9e49a6dbd1e66e22376bcea33978c34c
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-02-29 22:35:31 -0300

    porta correta

--- modules/config/sql.js
+++ modules/config/sql.js
@@ -4,7 +4,7 @@ module.exports = function() {
     return mysql.createConnection({
     host: "localhost",
     user: "root",
-    port: "3308",
+    port: "3306",
     password: "",
     database: "carona_solidaria"
 })};
